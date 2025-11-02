e30e0286f734d1d5eb304855d83e79ce587a709d
GitLoverArthur96 <nguat030@mymail.unisa.edu.au>
2025-11-02 14:01:53 +1030

    set up React version of Play/Stop and setsongText as well as rename class and for in the html

--- src/Components/DJControls.jsx
+++ src/Components/DJControls.jsx
@@ -3,28 +3,28 @@ function DJControls() {
         <>
             <div className="input-group flex-nowrap">
                 <span className="input-group-text" id="cpm_input_box">SetCPM</span>
-                <input type="text" class="form-control" placeholder="120" aria-label="cpm" aria-describedby="cpm_input_box" />
+                <input type="text" className="form-control" placeholder="120" aria-label="cpm" aria-describedby="cpm_input_box" />
             </div>
 
             <br />
 
-            <label for="volume_range" class="form-label">Set Volume</label>
-            <input type="range" class="form-range" min="0" max="1" step="0.01" id="volume_range"></input>
+            <label htmlFor="volume_range" className="form-label">Set Volume</label>
+            <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"></input>
 
             <br/>
 
-            <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
-                <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+            <div className="form-check form-switch">
+                <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Default switch checkbox input</label>
             </div>
-            <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked"  />
-                <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+            <div className="form-check form-switch">
+                <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked"  />
+                <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Checked switch checkbox input</label>
             </div>
 
-            <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked"  />
-                <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+            <div className="form-check form-switch">
+                <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked"  />
+                <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Checked switch checkbox input</label>
             </div>
 
 
