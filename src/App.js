85521c245c05e2193e75a4bc3129ca681228ee6e
GitLoverArthur96 <nguat030@mymail.unisa.edu.au>
2025-11-04 21:57:33 +1030

    Create box around strudel code

--- src/App.js
+++ src/App.js
@@ -21,50 +21,7 @@ const handleD3Data = (event) => {
     console.log(event.detail);
 };
 
-// export function SetupButtons() {
 
-//     document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
-//     document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
-//     document.getElementById('process').addEventListener('click', () => {
-//         Proc()
-//     }
-//     )
-//     document.getElementById('process_play').addEventListener('click', () => {
-//         if (globalEditor != null) {
-//             Proc()
-//             globalEditor.evaluate()
-//         }
-//     }
-//     )
-// }
-
-
-
-// export function ProcAndPlay() {
-//     if (globalEditor != null && globalEditor.repl.state.started == true) {
-//         console.log(globalEditor)
-//         Proc()
-//         globalEditor.evaluate();
-//     }
-// }
-
-// export function Proc() {
-
-//     let proc_text = document.getElementById('proc').value
-//     let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
-//     ProcessText(proc_text);
-//     globalEditor.setCode(proc_text_replaced)
-// }
-
-// export function ProcessText(match, ...args) {
-
-//     let replace = ""
-//     // if (document.getElementById('flexRadioDefault2').checked) {
-//     //     replace = "_"
-//     // }
-
-//     return replace
-// }
 
 export default function StrudelDemo() {
 
@@ -96,6 +53,10 @@ export default function StrudelDemo() {
         }, 100);
         
     };
+
+
+   
+    const [cpm , setCpm] = useState(120);
     
 
 useEffect(() => {
@@ -132,8 +93,14 @@ useEffect(() => {
             });
             
         document.getElementById('proc').value = stranger_tune
-        // SetupButtons()
-        // Proc()
+       
+
+
+      
+        
+        // if (globalEditor.current) {
+        //     globalEditor.replace(cpm, ${cpm})
+        // }
     }
 
     globalEditor.setCode(songText);
@@ -147,9 +114,13 @@ return (
 
             <div className="container-fluid"> 
                 <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <ProcTextArea Value={songText} onChange={(e) => setSongText(e.target.value)}/>
-                    </div>
+                    
+                        <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                            <div className="section-box">
+                                <h3 className="section-title">TESTT</h3>
+                                <ProcTextArea Value={songText} onChange={(e) => setSongText(e.target.value)}/>
+                            </div>
+                        </div>
                     <div className="col-md-4">
 
                         <nav>
